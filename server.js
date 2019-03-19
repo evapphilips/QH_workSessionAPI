495232f5bb64d9998c6d369e91a84a7f30202708
evapphilips <ep1521@nyu.edu>
2019-03-19 19:01:47 -0400

    fixing multiple variables

--- server.js
+++ server.js
@@ -27,18 +27,17 @@ const Schema = mongoose.Schema;
 // const HelloSchema = new Schema({message:String});
 // const HelloModel = mongoose.model('Hello', HelloSchema)
 const HelloSchema = new Schema({
-    message: String,
-    task: String,
-    project: String,
-    place: String,
-    goal: String,
-    preexcitement: String,
-    precoffee: Int,
-    prehunger: String,
-    location: String,
-    weather: String,
-    sound: String
-
+    message:String,
+    task:String,
+    project:String,
+    place:String,
+    goal:String,
+    preexcitement:String,
+    precoffee:Int,
+    prehunger:String,
+    location:String,
+    weather:String,
+    sound:String
 })
 const HelloModel = mongoose.model('Hello', HelloSchema)
 
